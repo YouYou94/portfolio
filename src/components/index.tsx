9955b9a2a442f63843b6935cec44a8a3b52dbe7e
YouYou94 <ybh4115@gmail.com>
2023-05-23 20:46:09 +0900

    Export Component

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export { Header } from './header/Header.Container';
+export { Cover } from './cover/Cover.Container';
+export { About } from './about/About.Container';
+export { Experience } from './experience/Experience.Container';
+export { Contact } from './contact/Contact.Container';
