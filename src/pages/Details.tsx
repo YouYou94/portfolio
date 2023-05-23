cd2fee5c617182726b7c7c71987a948b7ea0e9b3
YouYou94 <ybh4115@gmail.com>
2023-05-23 20:46:27 +0900

    Add: Pages

---
+++ src/pages/Details.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+const Details = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default Details;
---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,14 @@
+import { About, Contact, Cover, Experience } from '../components';
+
+const Portfolio = () => {
+  return (
+    <>
+      <Cover />
+      <About />
+      <Experience />
+      <Contact />
+    </>
+  );
+};
+
+export default Portfolio;
