690af452e8cdd35f283efc8e0ae980b84ec068ae
YouYou94 <ybh4115@gmail.com>
2023-05-24 17:35:33 +0900

    Stlye: Cover Presenter 구현

--- src/components/cover/Cover.Presenter.tsx
+++ src/components/cover/Cover.Presenter.tsx
@@ -1,5 +1,43 @@
-import { Section } from './CoverStyled';
+import { TagSpan } from '../common/Span';
+import {
+  Section,
+  CoverBox,
+  TitleBox,
+  Title,
+  HeaderBox,
+  HeaderTitle,
+} from './CoverStyled';
 
 export const CoverUI = () => {
-  return <Section>커버 100vh</Section>;
+  return (
+    <Section id="1">
+      <CoverBox>
+        <TagSpan tag="!DOCTYPE" variable="html" isOpen={true} />
+        <TagSpan tag="html" isOpen={true} />
+        <TagSpan tag="head" isOpen={true} />
+        <TagSpan
+          tag="meta"
+          variable="charset"
+          value="UTF-8"
+          isOpen={true}
+          paddingLeft="3rem"
+        />
+        <TagSpan tag="title" isOpen={true} paddingLeft="3rem" />
+        <TitleBox>
+          <Title>포트폴리오</Title>
+        </TitleBox>
+        <TagSpan tag="title" isOpen={false} paddingLeft="3rem" />
+        <TagSpan tag="head" isOpen={false} />
+        <TagSpan tag="body" isOpen={true} />
+        <TagSpan tag="header" isOpen={true} paddingLeft="3rem" />
+        <HeaderBox>
+          <TagSpan tag="span" isOpen={true} paddingLeft="7rem" />
+          <HeaderTitle>YouYou94 | Frontend Developer</HeaderTitle>
+          <TagSpan tag="span" isOpen={true} />
+        </HeaderBox>
+        <TagSpan tag="header" isOpen={false} paddingLeft="3rem" />
+        <TagSpan tag="main" isOpen={true} paddingLeft="3rem" />
+      </CoverBox>
+    </Section>
+  );
 };
--- src/components/cover/CoverStyled.tsx
+++ src/components/cover/CoverStyled.tsx
@@ -1,8 +1,55 @@
 import styled from 'styled-components';
 
-export const Section = styled.div`
+export const Section = styled.section`
   width: 100%;
   height: 100vh;
 
-  padding-top: 5rem;
+  display: flex;
+  justify-content: center;
+
+  padding: 5rem 1rem 1rem 1rem;
+
+  background-color: #222831;
+`;
+
+export const CoverBox = styled.div`
+  width: 30rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const TitleBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Title = styled.span`
+  font-family: 'GowunDodum';
+  font-size: 5rem;
+
+  color: #eeeeee;
+
+  &:hover {
+    color: #ffd369;
+  }
+`;
+
+export const HeaderBox = styled.div`
+  display: flex;
+`;
+
+export const HeaderTitle = styled.span`
+  margin: 0 0.3rem;
+
+  font-family: 'GowunDodum';
+
+  color: #eeeeee;
+
+  text-align: end;
+
+  &:hover {
+    color: #ffd369;
+  }
 `;
