a6dab34f736f286769ad7880a4c9e1be184e6771
YouYou94 <ybh4115@gmail.com>
2023-05-24 20:15:37 +0900

    Stlye: Header Presenter 구현 / React-Scroll 적용

--- src/components/header/Header.Presenter.tsx
+++ src/components/header/Header.Presenter.tsx
@@ -1,5 +1,26 @@
-import { Header } from './HeaderStyled';
+import { Link } from 'react-scroll';
+import { Header, LogoBox, NavBox, Nav, List } from './HeaderStyled';
 
 export const HeaderUI = () => {
-  return <Header>헤더 5rem</Header>;
+  return (
+    <Header>
+      <LogoBox>로고</LogoBox>
+      <NavBox>
+        <Nav>
+          <Link to="1" spy={true} smooth={true}>
+            <List>Home</List>
+          </Link>
+          <Link to="2" spy={true} smooth={true}>
+            <List>About</List>
+          </Link>
+          <Link to="3" spy={true} smooth={true}>
+            <List>Project</List>
+          </Link>
+          <Link to="4" spy={true} smooth={true}>
+            <List>Contact</List>
+          </Link>
+        </Nav>
+      </NavBox>
+    </Header>
+  );
 };
--- src/components/header/HeaderStyled.tsx
+++ src/components/header/HeaderStyled.tsx
@@ -5,4 +5,47 @@ export const Header = styled.header`
 
   width: 100%;
   height: 5rem;
+
+  display: flex;
+  justify-content: space-between;
+
+  padding: 0 5rem;
+
+  background-color: #222831;
+  color: #eeeeee;
+
+  // 반응형 웹
+  @media screen and (max-width: 768px) {
+    padding: 0 1rem;
+  }
+`;
+
+export const LogoBox = styled.div`
+  width: 5rem;
+`;
+
+export const Logo = styled.img``;
+
+export const NavBox = styled.nav`
+  width: 20rem;
+
+  display: flex;
+`;
+
+export const Nav = styled.ul`
+  flex: 1;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const List = styled.li`
+  font-family: 'GowunDodum';
+
+  cursor: pointer;
+
+  &:hover {
+    color: #ffd369;
+  }
 `;
