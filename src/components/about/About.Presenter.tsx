cbd8d5def854cf99269e4fbcfefda87f36059c65
YouYou94 <ybh4115@gmail.com>
2023-05-31 18:00:57 +0900

    Mod: About Component 소개 및 기술스택 디자인 변경

--- src/components/about/About.Presenter.tsx
+++ src/components/about/About.Presenter.tsx
@@ -1,12 +1,15 @@
 import { CloseTag, OpenTag, VariableTag } from '../common/Span';
 import {
   About,
-  Br,
   TitleBox,
   Title,
   IntrduceBox,
-  IntroduceSpan,
   TechBox,
+  IntroduceTitle,
+  IntroduceSubTitle,
+  EmpasisSpan,
+  IntroduceSpan,
+  TechBadge,
 } from './AboutStyled';
 
 export const AboutUI = () => {
@@ -23,27 +26,59 @@ export const AboutUI = () => {
       <CloseTag tag="h1" paddingLeft="4" />
       <OpenTag tag="p" paddingLeft="4" />
       <IntrduceBox>
+        <IntroduceTitle>
+          " 프론트엔드 개발자를 꿈 꾸는 유병현입니다. "
+        </IntroduceTitle>
         <IntroduceSpan>
-          작은 변화에도 빠르게 결과를 보여주는 매력에 빠진 프론트엔드 개발자
-          유병현입니다.
+          <EmpasisSpan>작은 변화에도 다른 결과</EmpasisSpan>를 보여주는 매력을
+          가진 프론트엔드 개발이 즐겁습니다. <EmpasisSpan>꾸준함</EmpasisSpan>은
+          저의 가장 강한 무기이며, 이를 바탕으로 역량을 키우고 있습니다. 저는
+          사용자에게 <EmpasisSpan>1초도 아깝지 않은 컨텐츠를 제공</EmpasisSpan>{' '}
+          하는 것이 목표입니다.
         </IntroduceSpan>
         <IntroduceSpan>
-          사용자에게 1초도 아깝지 않은 컨텐츠를 제공하는 것이 저의 목표입니다.
+          저는 <EmpasisSpan>New를 두려워하지 않고</EmpasisSpan> 적응할 자신이
+          있습니다. 스스로 많이 부족하다고 느끼고 있습니다. 모르는 것은 알기
+          위해 <EmpasisSpan>물음표</EmpasisSpan>, 배운 것은 기억하기 위해{' '}
+          <EmpasisSpan>느낌표</EmpasisSpan>의 자세를 가지겠습니다. 이런
+          마음가짐으로 <EmpasisSpan>어제보다 오늘 더 성장하는</EmpasisSpan>{' '}
+          개발자가 되기 위해 노력하는 유병현이 되겠습니다.
         </IntroduceSpan>
+        <IntroduceSubTitle>기술스택</IntroduceSubTitle>
+        <TechBox>
+          <TechBadge
+            backgroundcolor="#3178C6"
+            title="Portfolio | Offline Memo App"
+          >
+            Typescript
+          </TechBadge>
+          <TechBadge backgroundcolor="#F7DF1E" title="Bujolog | Order App">
+            Javascript
+          </TechBadge>
+          <TechBadge backgroundcolor="#E34F26">HTML</TechBadge>
+          <TechBadge backgroundcolor="#1572B6">CSS</TechBadge>
+        </TechBox>
+        <TechBox>
+          <TechBadge backgroundcolor="#61DAFB">React</TechBadge>
+          <TechBadge backgroundcolor="#764ABC" title="Bujolog">
+            Redux-Toolkit
+          </TechBadge>
+          <TechBadge
+            backgroundcolor="#3578E5"
+            title="Portfolio | Offline Memo App"
+          >
+            Recoil
+          </TechBadge>
+          <TechBadge backgroundcolor="#61DAFB" title="Order App">
+            Context API
+          </TechBadge>
+        </TechBox>
+        <TechBox>
+          <TechBadge backgroundcolor="#FFCA28">Firebase</TechBadge>
+          <TechBadge backgroundcolor="#DB7093">Styled-Components</TechBadge>
+        </TechBox>
       </IntrduceBox>
       <CloseTag tag="p" paddingLeft="4" />
-
-      <Br />
-
-      <OpenTag tag="h1" paddingLeft="4" />
-      <TitleBox>
-        <Title>기술스택</Title>
-      </TitleBox>
-      <CloseTag tag="h1" paddingLeft="4" />
-      <OpenTag tag="div" paddingLeft="4" />
-      <TechBox></TechBox>
-      <CloseTag tag="div" paddingLeft="4" />
-      <CloseTag tag="section" paddingLeft="2" />
     </About>
   );
 };
--- src/components/about/AboutStyled.tsx
+++ src/components/about/AboutStyled.tsx
@@ -14,30 +14,6 @@ export const About = styled.section`
   }
 `;
 
-export const Br = styled.br``;
-
-export const Span = styled.span`
-  font-family: 'GowunDodum';
-
-  user-select: none;
-`;
-
-export const Title = styled(Span)`
-  font-size: 3rem;
-
-  &:hover {
-    color: #ffd369;
-  }
-`;
-
-export const IntroduceSpan = styled(Span)`
-  font-size: 1.5rem;
-
-  @media screen and (max-width: 768px) {
-    font-size: 1rem;
-  }
-`;
-
 export const Box = styled.div`
   display: flex;
   flex-direction: column;
@@ -52,9 +28,91 @@ export const IntrduceBox = styled(Box)`
   flex: 1;
 
   justify-content: center;
-  align-items: center;
+
+  padding: 0 6rem 0 6rem;
+
+  gap: 1rem;
+
+  @media screen and (max-width: 768px) {
+    padding: 0 3rem 0 3rem;
+  }
 `;
 
 export const TechBox = styled(Box)`
-  flex: 1;
+  flex-direction: row;
+
+  gap: 0.5rem;
+`;
+
+export const TechBadge = styled.div<{ backgroundcolor: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 0.25rem;
+  padding: 0.1rem 0.5rem;
+
+  background-color: ${(prop) => prop.backgroundcolor};
+  color: #222831;
+
+  font-family: 'Alkatra';
+  font-size: 1rem;
+
+  transition: all 0.75s;
+
+  &:hover {
+    transform: translateY(-0.125rem);
+  }
+`;
+
+export const Span = styled.span`
+  display: inline-block;
+
+  font-family: 'GowunDodum';
+
+  user-select: none;
+`;
+
+export const Title = styled(Span)`
+  font-size: 3rem;
+
+  &:hover {
+    color: #ffd369;
+  }
+`;
+
+export const IntroduceTitle = styled(Span)`
+  font-size: 1.75rem;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+`;
+
+export const IntroduceSubTitle = styled(Span)`
+  font-size: 1.5rem;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.25rem;
+  }
+`;
+
+export const IntroduceSpan = styled(Span)`
+  font-size: 1.25rem;
+
+  line-height: 1.75;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1rem;
+  }
+`;
+
+export const EmpasisSpan = styled(Span)`
+  transition: all 0.75s;
+
+  ${IntroduceSpan}:hover & {
+    color: #ffd369;
+
+    transform: translateY(-0.125rem);
+  }
 `;
