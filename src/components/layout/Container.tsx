c3748512722ce79e49282c5266deb87034529c0a
YouYou94 <ybh4115@gmail.com>
2023-06-05 17:14:19 +0900

    design(mode prop): 다크모드 prop 추가 및 디자인

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -4,18 +4,36 @@ import styled from 'styled-components';
 type ContainerProps = {
   mode: boolean;
   direction: string;
+  responsive?: number;
+  responsivedirection?: string;
   children: string | ReactElement | ReactElement[];
 };
 
-export const Container = ({ mode, direction, children }: ContainerProps) => {
+export const Container = ({
+  mode,
+  direction,
+  responsive,
+  responsivedirection,
+  children,
+}: ContainerProps) => {
   return (
-    <Section direction={direction} mode={mode.toString()}>
+    <Section
+      direction={direction}
+      mode={mode.toString()}
+      responsive={responsive}
+      responsivedirection={responsivedirection}
+    >
       {children}
     </Section>
   );
 };
 
-const Section = styled.section<{ direction: string; mode: string }>`
+const Section = styled.section<{
+  direction: string;
+  mode: string;
+  responsive?: number;
+  responsivedirection?: string;
+}>`
   position: relative;
 
   width: 100%;
@@ -23,9 +41,15 @@ const Section = styled.section<{ direction: string; mode: string }>`
 
   display: flex;
   flex-direction: ${(prop) => prop.direction};
+  justify-content: center;
+  align-items: center;
 
   background-color: ${(prop) =>
     prop.mode === 'true' ? 'rgb(34, 40, 49)' : 'rgb(255, 255, 255)'};
   color: ${(prop) =>
     prop.mode === 'true' ? 'rgb(238, 238, 238)' : 'rgb(34, 40, 49)'};
+
+  @media screen and (max-width: ${(prop) => prop.responsive}px) {
+    flex-direction: ${(prop) => prop.responsivedirection};
+  }
 `;
