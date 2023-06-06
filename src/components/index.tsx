d59d5b34a3ff9f4c0d063fdd226e626c90ecf4c8
YouYou94 <ybh4115@gmail.com>
2023-06-06 19:32:45 +0900

    :fire:

--- src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-/* Common */
-export { Header } from './common/header/Header';
-
-/* Layout */
-export { Container } from './layout/Container';
-
-/* Components */
-export { Cover } from './cover/Cover';
-export { Mouse } from './cover/Mouse';
