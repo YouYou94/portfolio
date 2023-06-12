ad218fb495426e3134d9842a2ed1b2f7810f8c90
YouYou94 <ybh4115@gmail.com>
2023-06-12 15:46:53 +0900

    :fire: Footer Component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,7 +3,6 @@ export { Header } from './header/Header';
 export { Intro } from './intro/Intro';
 export { Experience } from './experience/Experience';
 export { Contact } from './contact/Contact';
-export { Footer } from './footer/Footer';
 
 /* Common */
 export { Template } from './common/Template';
