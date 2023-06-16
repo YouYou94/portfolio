8cfa39f018f635aa212855bd263e427a80539dbc
YouYou94 <ybh4115@gmail.com>
2023-06-16 16:39:24 +0900

    Feat: 스크롤 시 한 영역 씩 내려가는 이벤트 기능 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,26 +1,94 @@
 import { styled } from 'styled-components';
-import { Cover, Introduce, Contact } from '../components';
-import { useState } from 'react';
+import {
+  Cover,
+  Introduce,
+  Contact,
+  Experience,
+  CommonHeader,
+} from '../components';
+import { useEffect, useRef } from 'react';
 import { useRecoilValue } from 'recoil';
 import { getMode } from '../recoil';
 
-// 스크롤 시 한 영역 씩 스크롤 다운
-// 헤더는 공통으로하고 fixed
-// -> 이렇게 되면 자연스레 Header는 공통 컴포넌트로 빠지고
-// Nav 없애기
-// -> Nav 없애는 대신 맨 위에 추가하고 스크롤 시 옆에 하나 추가
-
 const Main = () => {
   const mode = useRecoilValue(getMode);
+  const pageRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = (event: WheelEvent) => {
+      event.preventDefault();
+
+      const deltaY = event.deltaY;
+      const clientHeight = pageRef.current?.clientHeight || 0;
+      const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+
+      /* 스크롤 === 세로길이 */
+      const scrollTop = Math.max(
+        document.documentElement.scrollTop,
+        document.body.scrollTop,
+      );
+
+      const handleScrollTo = (count: number) => {
+        window.scrollTo({
+          top: pageHeight * count,
+          behavior: 'smooth',
+        });
+      };
+
+      if (deltaY > 0) {
+        if (scrollTop >= 0 && scrollTop < clientHeight / 4) {
+          handleScrollTo(1);
+        } else if (
+          scrollTop >= clientHeight / 4 &&
+          scrollTop < (clientHeight / 4) * 2
+        ) {
+          handleScrollTo(2);
+        } else if (
+          scrollTop >= (clientHeight / 4) * 2 &&
+          scrollTop < (clientHeight / 4) * 3
+        ) {
+          handleScrollTo(3);
+        }
+      } else if (deltaY < 0) {
+        if (
+          scrollTop >= clientHeight / 4 &&
+          scrollTop < (clientHeight / 4) * 2
+        ) {
+          handleScrollTo(0);
+        } else if (
+          scrollTop >= (clientHeight / 4) * 2 &&
+          scrollTop < (clientHeight / 4) * 3
+        ) {
+          handleScrollTo(1);
+        } else if (
+          scrollTop >= (clientHeight / 4) * 3 &&
+          scrollTop < (clientHeight / 4) * 4
+        ) {
+          handleScrollTo(2);
+        }
+      }
+    };
+
+    /* 이벤트 추가 */
+    pageRef.current?.addEventListener('wheel', handleScroll, {
+      passive: false,
+    });
+
+    /* 이벤트 제거 */
+    return () => {
+      pageRef.current?.removeEventListener('wheel', handleScroll);
+    };
+  }, []);
 
   return (
-    <Layout mode={mode.toString()}>
+    <Layout ref={pageRef} mode={mode.toString()}>
+      <CommonHeader />
       {/* 커버 */}
       <Cover />
       {/* 소개 */}
       <Introduce />
       {/* 프로젝트 */}
-      <></>
+      <Experience />
       {/* 연락 */}
       <Contact />
     </Layout>
