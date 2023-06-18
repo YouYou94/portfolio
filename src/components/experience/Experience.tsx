1bc62f3a58d5eb48305e455f9bf22ff20def3987
YouYou94 <ybh4115@gmail.com>
2023-06-18 17:44:41 +0900

    Add: Project Component

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,15 +1,57 @@
-import * as S from './ExperienceStyled';
+import styled from 'styled-components';
+import { ProjectList } from '../../constants/index';
 import { Project } from './Project';
+import { useRecoilValue } from 'recoil';
+import { getMode } from '../../recoil';
 
 export const Experience = () => {
+  const mode = useRecoilValue(getMode);
+
   return (
-    <S.Layout>
-      <S.TitleBox>
-        <S.Title>Project Experience</S.Title>
-      </S.TitleBox>
-      <S.ProjectBox>
-        <Project />
-      </S.ProjectBox>
-    </S.Layout>
+    <Layout>
+      <ProjectBox>
+        {ProjectList?.map((project: any, index: number) => {
+          const { ui, title, subtitle, about, techstack, link } = project;
+          return (
+            <Project
+              key={index}
+              mode={mode}
+              ui={ui}
+              title={title}
+              subtitle={subtitle}
+              about={about}
+              techstack={techstack}
+              link={link}
+            />
+          );
+        })}
+      </ProjectBox>
+    </Layout>
   );
 };
+
+const Layout = styled.section`
+  height: 100vh;
+
+  padding: 2rem 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProjectBox = styled.div`
+  width: 48rem;
+
+  display: grid;
+
+  grid-template-columns: 1fr 1fr;
+
+  gap: 1rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+
+    grid-template-columns: 1fr 1fr;
+  }
+`;
--- src/components/experience/Project.tsx
+++ src/components/experience/Project.tsx
@@ -1,26 +1,58 @@
-import styled from 'styled-components';
-import a from '../../assets/project/portfolio.png';
+import React from 'react';
+import * as S from './ProjectStyled';
 
-export const Project = () => {
-  return (
-    <Box>
-      <UI uiurl={a} />
-    </Box>
-  );
+interface LinkType {
+  github: string;
+  demo: string;
+}
+
+type ProjectProps = {
+  mode: boolean;
+  ui: string;
+  title: string;
+  subtitle: string;
+  about: string;
+  techstack: string;
+  link: LinkType;
 };
 
-const Box = styled.article`
-  display: flex;
-  flex-direction: column;
-
-  gap: 1rem;
-`;
-
-const UI = styled.div<{ uiurl: string }>`
-  aspect-ratio: 4 / 3;
-
-  background-image: url(${(prop) => prop.uiurl});
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-`;
+export const Project = ({
+  mode,
+  ui,
+  title,
+  subtitle,
+  about,
+  techstack,
+  link,
+}: ProjectProps) => {
+  return (
+    <S.Box>
+      <S.UI mode={mode.toString()}>
+        <S.UIimg src={ui} alt={subtitle} />
+      </S.UI>
+      <S.ProjectBox>
+        <S.ContentBox>
+          <S.Title>
+            {title}: <S.Span>{subtitle}</S.Span>
+          </S.Title>
+          <S.Span>{about}</S.Span>
+          <S.Span>
+            <strong>사용기술</strong> : {techstack}
+          </S.Span>
+        </S.ContentBox>
+        <S.LinkBox mode={mode.toString()}>
+          <S.Button>
+            <S.Link href={link.github} target="_blank">
+              깃허브
+            </S.Link>
+          </S.Button>
+          <S.Button>
+            <S.Link href={link.demo} target="_blank">
+              사이트
+            </S.Link>
+          </S.Button>
+        </S.LinkBox>
+      </S.ProjectBox>
+    </S.Box>
+  );
+};
