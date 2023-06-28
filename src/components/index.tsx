5a48aa59b218ff7415dd68acfb420a502d3a103b
YouYou94 <ybh4115@gmail.com>
2023-06-28 17:01:12 +0900

    init: Template(#1: main Tag / darkmode background-color color)

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,12 @@
 /* Common */
 export { CommonHeader } from './common/CommonHeader';
 export { Header } from './common/header/index';
+export { Template } from './common/Template';
 
 /* Components */
 export { Intro } from './intro/Intro';
+
+export { Footer } from './footer/index';
 
 export { Introduce } from './introduce/Introduce';
 export { Experience } from './experience/Experience';
