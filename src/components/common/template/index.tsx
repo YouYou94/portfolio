d6a5f6bb8d31e344ddf8ded3401ed7bba960354c
YouYou94 <ybh4115@gmail.com>
2023-06-29 18:53:47 +0900

    init: Template Component

---
+++ src/components/common/template/index.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { ReactElement } from 'react';
+import { useRecoilValue } from 'recoil';
+import { getMode } from '../../../recoil';
+
+interface TemplateProps {
+  children: string | ReactElement | ReactElement[];
+}
+
+export const Template = ({ children }: TemplateProps) => {
+  const mode = useRecoilValue(getMode);
+
+  return <Layout mode={mode.toString()}>{children}</Layout>;
+};
+
+const Layout = styled.main<{ mode: string }>`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  background-color: ${(prop) =>
+    prop.mode === 'true' ? 'rgb(34, 40, 49)' : ''};
+
+  color: ${(prop) =>
+    prop.mode === 'true' ? 'rgb(255, 255, 255)' : 'rgb(34, 40, 49)'};
+`;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,3 @@
 /* Common Components */
 export { Header } from './common/header';
+export { Template } from './common/template';
