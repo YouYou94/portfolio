6a5976f787aeb2b340366c73dd17a847097cf1b4
YouYou94 <ybh4115@gmail.com>
2023-07-02 18:46:14 +0900

    feat: 스크롤 시 Top 버튼 생성

--- src/components/template/index.tsx
+++ src/components/template/index.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
-import { ReactElement } from 'react';
+import { ReactElement, useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { getMode } from '../../recoil';
+import { BiArrowToTop } from 'react-icons/bi';
 
 interface TemplateProps {
   children: string | ReactElement | ReactElement[];
@@ -9,8 +10,30 @@ interface TemplateProps {
 
 export const Template = ({ children }: TemplateProps) => {
   const mode = useRecoilValue(getMode);
+  const [scrollPercent, setScrollPercent] = useState<number>(0);
 
-  return <Layout mode={mode.toString()}>{children}</Layout>;
+  const handleClickUp = () =>
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+
+  const updateScroll = () => {
+    setScrollPercent(window.scrollY || document.documentElement.scrollTop);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', updateScroll);
+  });
+
+  return (
+    <Layout mode={mode.toString()}>
+      {children}
+      <TopBtnBox scroll={scrollPercent} onClick={handleClickUp}>
+        <BiArrowToTop size={32} />
+      </TopBtnBox>
+    </Layout>
+  );
 };
 
 const Layout = styled.main<{ mode: string }>`
@@ -26,4 +49,26 @@ const Layout = styled.main<{ mode: string }>`
 
   color: ${(prop) =>
     prop.mode === 'true' ? 'rgb(255, 255, 255)' : 'rgb(34, 40, 49)'};
+`;
+
+const TopBtnBox = styled.div<{ scroll: number }>`
+  position: fixed;
+
+  right: 1rem;
+  bottom: 1rem;
+
+  width: 4rem;
+  height: 4rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 50%;
+
+  background-color: rgb(204, 204, 204, 0.3);
+
+  cursor: pointer;
+
+  ${(prop) => (prop.scroll > 100 ? '' : 'display: none')};
 `;
